c286f646fb63b23712dd3051e97a2b425f90a15a
Tim Snyder <tsnyder701@gmail.com>
2026-03-20 15:37:39 -0400

    Add shooter/indexer/intake telemetry logging and timestamped hoot log files

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -98,7 +98,9 @@ public class TunerConstants {
 
     // CAN bus that the devices are located on;
     // All swerve devices must share the same CAN bus
-    public static final CANBus kCANBus = new CANBus("", "./logs/example.hoot");
+    public static final CANBus kCANBus = new CANBus("",
+        "./logs/match_" + java.time.LocalDateTime.now()
+            .format(java.time.format.DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")) + ".hoot");
 
     // Theoretical free speed (m/s) at 12 V applied output;
     // This needs to be tuned to your individual robot
